8261661692a3ab3a641d6bf286cf3efe2a53ff65
DevFl4me <163850171+DevFl4me@users.noreply.github.com>
2024-04-18 09:48:05 -0300

    Introdução JavaScript

---
+++ JavaScript/materialDeAula/script/script.js
@@ -0,0 +1,15 @@
+console.log("Olá, mundoooooo!");
+
+// caixas de texto (input)
+// prompt = recebe um texto
+// window.prompt("Qual é seu nome?");
+
+// alert = manda uma mensagem
+// window.alert("FRONT É TOP");
+
+// confirm = pergunta podendo sair verdadeiro ou falso
+// window.confirm("Deseja baixar arquivo novamente?");
+
+var nome = "Ryan";
+var ligado = true;
+var numero = 91;
