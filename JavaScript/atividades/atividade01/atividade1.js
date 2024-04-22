ae88437cb8a5ed2d96d0f820a79507fe99356aee
DevFl4me <163850171+DevFl4me@users.noreply.github.com>
2024-04-22 11:46:19 -0300

    Patch Fix

--- JavaScript/atividades/atividade01/atividade1.js
+++ JavaScript/atividades/atividade01/atividade1.js
@@ -1,19 +1,29 @@
 // faça dois scripts:
 // 1-calcule conta, faça uma aplicação js que pegue o número de clientes em uma mesa, o valor total da conta e após isso divida a conta de forma igual a todos os clientes
-
-
-// Exercicio 01
-var valorConta;
-var clientes;
-var valorTotalDividido;
+function atividade1(){
+    let valorConta;
+    let clientes;
+    let valorTotalDividido;
 
 window.alert("Seja bem vindo ao Divisor de contas");
 
- valorConta= Number (window.prompt("Digite o valor da conta a ser paga:R$"));
- clientes= Number (window.prompt("Quantas pessoas tem na mesa?"));
-
- valorTotalDividido = valorConta/clientes;
+    valorConta= Number (window.prompt("Digite o valor da conta a ser paga:R$"));
+    clientes= Number (window.prompt("Quantas pessoas tem na mesa?"));
+    valorTotalDividido = (valorConta/clientes).toFixed(2);
 
 window.alert("O valor dividido para cliente é de:R$"+valorTotalDividido)
 
-// Fim Exercicio 01
+}
+function atividade2(){
+    let numero;
+    let antecessor;
+    let sucessor; 
+
+window.alert("Bem-vindo")
+
+    numero = Number(window.prompt("Digite um numero:"))
+    antecessor = numero-1;
+    sucessor = numero+1;
+
+window.alert("O sucessor do numero é " +sucessor+" e o antecessor é "+antecessor)
+}
