7b9a3b9314a2b67612e70355b051b8c7b4e31f97
DevFl4me <163850171+DevFl4me@users.noreply.github.com>
2024-04-22 09:45:47 -0300

    Atualizando

--- JavaScript/materialDeAula/script/script.js
+++ JavaScript/materialDeAula/script/script.js
@@ -13,3 +13,44 @@ console.log("Olá, mundoooooo!");
 var nome = "Ryan";
 var ligado = true;
 var numero = 91;
+var objeto = [];
+// Objeto= tipo de coletanea de dados, onde podemos inserir,
+// remover e pesquisar.
+
+// type of = verifica o tipo da variavel
+
+const fixo="Nao posso mudar!";
+var temporaria="Sou temporário";
+let temporarioVar="Fico Salvo!";
+
+// var escopo global e sobrescrevivel
+// Let escopo local e sobrescrevivel
+// const escopo global e não sobrescrevivel
+
+function teste(){
+    temporaria="Sou temporário";
+    temporarioVar="fico Salvo";
+
+    console.log(temporaria);
+    console.log(temporarioVar);
+}
+
+// output = resultado
+// input = numero1,numero2
+// Coleta dois números e soma eles, após isso, imprime na tela o resultado.
+function somar(){
+    window.alert("Seja bem vindo ao algoritmo de somar!");
+    let numero1= Number(window.prompt("Digite o seu primeiro número:"));
+    let numero2= Number(window.prompt("Digite seu segundo número:"));
+    
+    // ParseInt = Numeros inteiros 
+    // ParseFloat = Números reais
+    // Number = Numeros inteiros e reais
+    
+    var resultado = numero1+numero2;
+
+    // concatenação
+    window.alert("Resultado é de:"+resultado);
+    window.alert(`Resultado é de:${resultado}`);
+}
+somar()
