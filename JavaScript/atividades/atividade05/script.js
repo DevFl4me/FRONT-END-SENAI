64f2f84126e3fef0782ff3b22f1beef557a91d6b
Ryan Pablo <ryanpablobarbosa123@gmail.com>
2024-04-26 15:42:04 -0300

    Iniciando exercicio

---
+++ JavaScript/atividades/atividade05/script.js
@@ -0,0 +1,11 @@
+// -Faça um sistema de adicionar peças de computador de forma fixa (Deixando as opções já pre selecionadas)
+//  e que elas no decorrer do tempo possam serem adicionas a um array chamado carrinho
+
+let valorTotal
+
+function adicionarProduto(valorProduto){
+    valorTotal +=valorProduto;
+
+}
+
+var carrinho = Array();
