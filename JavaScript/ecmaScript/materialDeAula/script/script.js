1f3af4914a277af72e89334f4dd6dbcf7f0c21dc
DevFl4me <163850171+DevFl4me@users.noreply.github.com>
2024-05-07 11:48:49 -0300

    ObjectEntries

--- JavaScript/ecmaScript/materialDeAula/script/script.js
+++ JavaScript/ecmaScript/materialDeAula/script/script.js
@@ -54,14 +54,35 @@ function pad(){
 };
 
 function objectEntries(){
+    const aluno = {
+        matricula : 180,
+        cursosFinalizados : ["Front","UX","Banco de dados"],
+        cursoAtual : "Dominó",
+        nome : "Raul",
+    };
 
-}
+    // retorna todos atributos do objeto (matricula, cursoFinalizados e outros.....)
+    var alunoKey = Object.keys(aluno);
+    console.log(alunoKey);
+    // retorna todos atributos e seus valores (matricula, 180/ cursosFinalizados, Array)
+    var alunoEntries = Object.entries(aluno);
+    console.log(alunoEntries)
+
+    // retorna somente valor (Domino, Array)
+    console.log(aluno.matricula);
+
+    //map - faz um processo para cada elemento do nosso array
+    var teste = ["Matricula","Front End"];
+    var mapResultado = teste.map((elemento)=>`<li>${elemento}</li>`)
+    console.log(mapResultado);
+};
 
 function promisses(){
     
-}
+};
 
 // executa functions
 replaceEcma();
 pesquisar();
 pad();
+objectEntries();
