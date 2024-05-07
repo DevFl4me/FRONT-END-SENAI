7e0c963dba281cb9bad4543d91c3e5e7f9a1e226
DevFl4me <163850171+DevFl4me@users.noreply.github.com>
2024-05-07 10:51:16 -0300

    Correcao Atividade Ecma

---
+++ JavaScript/ecmaScript/atividades/correcaoAtvd01/scripts/index.js
@@ -0,0 +1,46 @@
+const btnPesquisar = document.querySelector("#btnResposta");
+const jogos = ["Resident Evil","Dota 2","Free Fire","League of Legends","Castlevania"];
+
+btnPesquisar.addEventListener("click",pesquisar);
+
+function pesquisar(){
+    let jogoPesquisado = document.querySelector("#inputResposta").value;
+    let resposta = document.querySelector("#pesquisarResposta");
+
+    let respostaPesquisa = jogos.includes(jogoPesquisado);
+
+    if(respostaPesquisa == true){
+        resposta.innerHTML = `Jogo encontrado! Item da pesquisa: ${jogoPesquisado}`;
+    }else{
+        resposta.innerHTML=`Jogo ${jogoPesquisado} não foi encontrado!`;
+    }
+
+}
+
+// ATIVA BTN PARA SUBSTITUIR UMA PALAVRA
+const btnReplace = document.querySelector("#btnReplace");
+
+btnReplace.addEventListener("click",substituirUm);
+
+// ATIVA BTN PARA SUBSTITUIR TODAS PALAVRAS
+const btnReplaceAll = document.querySelector("#btnReplaceAll");
+
+btnReplaceAll.addEventListener("click",substituirTodos);
+
+function substituirUm(){
+    let texto = document.querySelector("#texto").value;
+    let acharTermo = document.querySelector("#acharTermo").value;
+    let trocarTermo = document.querySelector("#trocarTermo").value;
+
+   let textoAtualizado = texto.replace(acharTermo,trocarTermo);
+   document.querySelector("#texto").value = textoAtualizado;
+}
+
+function substituirTodos(){
+    let texto = document.querySelector("#texto").value;
+    let acharTermo = document.querySelector("#acharTermo").value;
+    let trocarTermo = document.querySelector("#trocarTermo").value;
+
+   let textoAtualizado = texto.replaceAll(acharTermo,trocarTermo);
+   document.querySelector("#texto").value = textoAtualizado;
+}
