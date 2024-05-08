f4b320f86717944f850e93d2313ca2a6f2300048
Ronaldobsb <163850171+DevFl4me@users.noreply.github.com>
2024-05-08 11:55:16 -0300

    Atividade Personagem

---
+++ JavaScript/ecmaScript/atividades/atividade02/index.js
@@ -0,0 +1,28 @@
+// Faça um site, onde você pode ter um personagem ao seu critério ( Um pokémon ou um protagonista)
+//  com seus diversos atributos ( como vida, dano ou powerups).
+//  e no html não só possamos acrescentar mais estatísticas a ele como possamos estar acompanhando sua evolução ( ver visualmente o acréscimo acontecendo ).
+
+class personagem{
+    constructor(vida,dano,armadura,agilidade){
+        this.vida = vida;
+        this.dano = dano;
+        this.armadura = armadura;
+        this.agilidade = agilidade;
+    }
+
+}
+
+var donatello = new personagem(100,150,200,40);
+
+
+function somarVida(vidaRecebida){
+
+    let curar = document.querySelector("#btn-+vida");
+
+        donatello.vida+=vidaRecebida; 
+
+    resultado = document.querySelector("#resultado-vida");
+    resultado.innerHtml=`${vida}`
+
+
+}
