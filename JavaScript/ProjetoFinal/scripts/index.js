4e82df01c6274c91a852e28cf036efaeb18da028
Ryan Pablo <ryanpablobarbosa123@gmail.com>
2024-05-16 13:10:55 -0300

    atualizando Header e continuando lógica

--- JavaScript/ProjetoFinal/scripts/index.js
+++ JavaScript/ProjetoFinal/scripts/index.js
@@ -1,27 +1,21 @@
 // 1- click
 // 2- pintar elemento
 // 3-
-// document.addEventListener('DOMContentLoaded',()=>{
-//     const assento=document.querySelectorAll('.assento');
-//     assento.forEach(assento=>{
-//         assento.addEventListener("click",() =>{
-//             if(!assento.classList.add('selected')){
-//                 assento.classList.add('selected');
-//                 assento.style.pointerEvents ='none';
-//             }
-//         });
-
-// });
 
 document.addEventListener('DOMContentLoaded', () => {
-    const assento= document.querySelectorAll('.assento');
-
+    const assento = document.querySelectorAll('.assento');
+    var assentoSelec=0;
     assento.forEach(assento => {
         assento.addEventListener('click', () => {
             if (!assento.classList.contains('selected')) {
                 assento.classList.add('selected');
                 assento.style.pointerEvents = 'none'; 
+                assentoSelec++;
+                console.log(assentoSelec);
+                let resposta = document.querySelector("#resposta");
+                resposta.innerHTML= `${assentoSelec}`;
             }
         });
     });
 });
+
