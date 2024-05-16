915e2c1300771059eccf02890ec84d93c77f5ce7
Ryan Pablo <ryanpablobarbosa123@gmail.com>
2024-05-15 22:18:23 -0300

    adicionando click de assentos

--- JavaScript/ProjetoFinal/scripts/index.js
+++ JavaScript/ProjetoFinal/scripts/index.js
@@ -1,7 +1,27 @@
 // 1- click
 // 2- pintar elemento
 // 3-
+// document.addEventListener('DOMContentLoaded',()=>{
+//     const assento=document.querySelectorAll('.assento');
+//     assento.forEach(assento=>{
+//         assento.addEventListener("click",() =>{
+//             if(!assento.classList.add('selected')){
+//                 assento.classList.add('selected');
+//                 assento.style.pointerEvents ='none';
+//             }
+//         });
 
-function mudarBackground(elemento) {
-    elemento.classList.toggle('assentoSelecionado');
-}
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const assento= document.querySelectorAll('.assento');
+
+    assento.forEach(assento => {
+        assento.addEventListener('click', () => {
+            if (!assento.classList.contains('selected')) {
+                assento.classList.add('selected');
+                assento.style.pointerEvents = 'none'; 
+            }
+        });
+    });
+});
